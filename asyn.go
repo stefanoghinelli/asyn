7a843253fa6f41095e002ebdf36f2c1d814719ee
Stefano Ghinelli <stefano.ghinelli@yahoo.com>
2023-08-02 23:43:10 +0200

    added main and init function

---
+++ asyn.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type Synonym struct {
+}
+
+func main() {
+	var rootCmd 
+	var word string
+	var results int
+}
+
+func getSynonyms(word string, results int) () {
+	return nil
+}
